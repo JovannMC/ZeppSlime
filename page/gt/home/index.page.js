9b82cecb67c39b7f8628be67c5873a138e7d9026
Maya <jovannmc@femboyfurry.net>
2026-01-18 11:40:01 +0300

    feat: display sensor data
    also installed vislog

--- page/gt/home/index.page.js
+++ page/gt/home/index.page.js
@@ -1,15 +1,16 @@
 import * as hmUI from "@zos/ui";
 import { log as Logger } from "@zos/utils";
+import VisLog from "@silver-zepp/vis-log";
 import { checkSensor, Accelerometer, Gyroscope, Battery } from "@zos/sensor";
 import { MACAddress, Quaternion, Vector } from "@slimevr/common";
 import {
-  BoardType,
-  FirmwareFeatureFlags,
-  MCUType,
-  RotationDataType,
-  SensorStatus,
-  SensorType,
-  UserAction,
+	BoardType,
+	FirmwareFeatureFlags,
+	MCUType,
+	RotationDataType,
+	SensorStatus,
+	SensorType,
+	UserAction,
 } from "@slimevr/firmware-protocol";
 
 import { getText } from "@zos/i18n";
@@ -17,6 +18,24 @@ import { getDeviceInfo } from "@zos/device";
 import { px } from "@zos/utils";
 
 let text = null;
+let sensorText = {
+	battery: "none",
+	accel: "none",
+	gyro: "none",
+};
+
+function round2(value) {
+	if (typeof value !== "number") return value;
+	return Math.round(value * 100) / 100;
+}
+
+function renderSensors() {
+	if (!text) return;
+	text.setProperty(
+		hmUI.prop.TEXT,
+		`Accel: ${sensorText.accel}\nGyro: ${sensorText.gyro}`,
+	);
+}
 
 // UDP communication not possible with ZeppOS and app, so alternate approaches needed
 
@@ -31,40 +50,143 @@ let text = null;
 // via UDP, which should have much lower latency than idea 1
 
 const log = Logger.getLogger("ZeppSlime");
+const vis = new VisLog("index.js")
 let DEVICE_WIDTH = 0;
 let DEVICE_HEIGHT = 0;
 
 Page({
-  onInit() {
-    log.debug("page onInit invoked");
-    const deviceInfo = getDeviceInfo();
-    DEVICE_WIDTH = deviceInfo.width;
-    DEVICE_HEIGHT = deviceInfo.height;
-    log.info(`Device: ${DEVICE_WIDTH}x${DEVICE_HEIGHT}`);
-    log.info(`MAC Address: ${MACAddress.random().toString()}`);
-  },
-  build() {
-    log.debug("page build invoked");
-    text = hmUI.createWidget(hmUI.widget.TEXT, {
-      text: getText("appName"),
-      x: px(42),
-      y: px(200),
-      w: DEVICE_WIDTH - px(42) * 2,
-      h: px(100),
-      color: 0xffffff,
-      text_size: px(36),
-      align_h: hmUI.align.CENTER_H,
-      align_v: hmUI.align.CENTER_V,
-      text_style: hmUI.text_style.WRAP,
-    });
+	onInit() {
+		vis.debug("page onInit invoked");
+		const deviceInfo = getDeviceInfo();
+		DEVICE_WIDTH = deviceInfo.width;
+		DEVICE_HEIGHT = deviceInfo.height;
+		vis.info(`Device: ${DEVICE_WIDTH}x${DEVICE_HEIGHT}`);
+		vis.info(`MAC Address: ${MACAddress.random().toString()}`);
+	},
+	build() {
+		vis.debug("page build invoked");
+		text = hmUI.createWidget(hmUI.widget.TEXT, {
+			text: getText("appName"),
+			x: px(42),
+			y: px(200),
+			w: DEVICE_WIDTH - px(42) * 2,
+			h: px(150),
+			color: 0xffffff,
+			text_size: px(24),
+			align_h: hmUI.align.CENTER_H,
+			align_v: hmUI.align.CENTER_V,
+			text_style: hmUI.text_style.WRAP,
+		});
 
-    setInterval(() => {
-      const date = new Date();
-      const timeString = date.toLocaleTimeString();
-      text.setProperty(hmUI.prop.TEXT, getText("appName") + "\n" + timeString);
-    }, 1000);
-  },
-  onDestroy() {
-    log.debug("page onDestroy invoked");
-  },
+		renderSensors();
+
+		// check sensor data
+		const accelAvailable = checkSensor(Accelerometer);
+		const gyroAvailable = checkSensor(Gyroscope);
+		const batteryAvailable = checkSensor(Battery);
+
+		vis.info(`Accelerometer available: ${accelAvailable}`);
+		vis.info(`Gyroscope available: ${gyroAvailable}`);
+		vis.info(`Battery available: ${batteryAvailable}`);
+
+		let accel = null;
+		let gyro = null;
+		let battery = null;
+
+		if (accelAvailable) {
+			try {
+				accel = new Accelerometer({ frequency: 50 });
+				accel.start();
+			} catch (e) {
+				vis.warn(`Failed to start accelerometer: ${e}`);
+				sensorText.accel = "none";
+				renderSensors();
+			}
+		}
+
+		if (gyroAvailable) {
+			try {
+				gyro = new Gyroscope({ frequency: 50 });
+				gyro.start();
+			} catch (e) {
+				vis.warn(`Failed to start gyroscope: ${e}`);
+				sensorText.gyro = "none";
+				renderSensors();
+			}
+		}
+
+		if (batteryAvailable) {
+			battery = new Battery();
+			try {
+				const initialBatteryLevel = battery.getCurrent();
+				vis.info(`Battery level: ${initialBatteryLevel}%`);
+				sensorText.battery = `${initialBatteryLevel}%`;
+				renderSensors();
+			} catch (e) {
+				vis.warn(`Failed to read battery: ${e}`);
+				sensorText.battery = "none";
+				renderSensors();
+			}
+
+			battery.onChange((level) => {
+				vis.info(`Battery level: ${level}%`);
+				sensorText.battery = `${level}%`;
+				renderSensors();
+			});
+		} else {
+			sensorText.battery = "none";
+			renderSensors();
+		}
+
+		if (accel) {
+			try {
+				const accelData = accel.getCurrent();
+				const { x, y, z } = accelData;
+				const roundX = round2(x);
+				const roundY = round2(y);
+				const roundZ = round2(z);
+				sensorText.accel = `${roundX} ${roundY} ${roundZ}`;
+				renderSensors();
+			} catch (e) {
+				vis.warn(`Failed to read accelerometer: ${e}`);
+				sensorText.accel = "none";
+				renderSensors();
+			}
+
+			accel.onChange((data) => {
+				sensorText.accel = `${round2(data.x)} ${round2(data.y)} ${round2(data.z)}`;
+				renderSensors();
+			});
+		} else {
+			sensorText.accel = "none";
+			renderSensors();
+		}
+
+		if (gyro) {
+			try {
+				const gyroData = gyro.getCurrent();
+				const { x, y, z } = gyroData;
+				const roundX = round2(x);
+				const roundY = round2(y);
+				const roundZ = round2(z);
+				sensorText.gyro = `${roundX} ${roundY} ${roundZ}`;
+				renderSensors();
+			} catch (e) {
+				vis.warn(`Failed to read gyroscope: ${e}`);
+				sensorText.gyro = "none";
+				renderSensors();
+			}
+
+			gyro.onChange((data) => {
+				sensorText.gyro = `${round2(data.x)} ${round2(data.y)} ${round2(data.z)}`;
+				renderSensors();
+			});
+		} else {
+			sensorText.gyro = "none";
+			renderSensors();
+		}
+	},
+	onDestroy() {
+		vis.debug("page onDestroy invoked");
+	},
 });
